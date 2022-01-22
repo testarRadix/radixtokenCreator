e2ec1406e08341206060af25d5b36c3fbd048898
Ardial <38439555+Qlause@users.noreply.github.com>
2022-01-22 18:40:33 +0900

    Create step3.py

---
+++ step3.py
@@ -0,0 +1,18 @@
+curl -d '' 
+payload =  json.dumps({
+    "method": "construction.finalize_transaction",
+    "params": {
+        "blob": "fill with blob that we get from step 1",
+        "signatureDER": "fill with signatured code",
+        "publicKeyOfSigner": "fill with your public key address",
+        "immediateSubmit":"true"
+    },
+    "id": 0
+}) 
+headers = {"Content-Type": "application/json"}
+url = "https://mainnet.radixdlt.com/construction" 
+
+response = requests.request("POST", url, headers=header, data=payload)
+
+# this will show information e.g TXID
+response.text
