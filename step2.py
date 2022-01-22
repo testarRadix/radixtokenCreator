b4138cf9f323cda45bca398fe2a8c6325be1fee8
Ardial <38439555+Qlause@users.noreply.github.com>
2022-01-22 18:37:59 +0900

    Create step2.py

---
+++ step2.py
@@ -0,0 +1,5 @@
+# sign 
+# dont delete the quote (')
+sk = ecdsa.SigningKey.from_string(bytearray.fromhex('your private key'), curve=ecdsa.SECP256k1)
+# this command will show signatured code that we will use at step 3
+print(sk.sign_digest(bytes.fromhex('blobhastosign'), sigencode=sigencode_der).hex())
